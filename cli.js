02ae24608725e40135ad244055144f7c5dbc928a
lukluk <luklukaha@gmail.com>
2015-03-09 08:37:12 +0700

    update exec script

--- cli.js
+++ cli.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 var readline = require('readline'),
     S = require('string'),
     request = require('request'),
@@ -7,25 +9,137 @@ var readline = require('readline'),
     cheerio = require('cheerio'),
     util = require('util')
 rl = readline.createInterface(process.stdin, process.stdout);
-console.log("WSQL v.Alpha");
-rl.setPrompt('WSQL> ');
-rl.prompt();
 var connected = false;
 var $ = null;
+var sandbox = {
+    send: function (line, cb) {
+        var sandbox = {
+            web: new wsql($),
+            $: $
+        }
+        try {
+            var vmResult = vm.runInNewContext(line, sandbox);
+            cb && cb();
+            console.log(vmResult);
+        } catch (e) {
+            console.log(e);
+        }
+
+    }
+}
+
+// FILE
+if (process.argv[2]) {
+    var txt = fs.readFileSync(process.argv[2]) + '';
+    var qlines = txt.split(';');
+    //    var qlines = [];
+    //    for (var n in lines) {
+    //        var line = lines[n];
+    //        var a = line.split(';');
+    //        if (a.length > 1) {
+    //            for (var l in a) {
+    //                qlines.push(a[l]);
+    //            }
+    //        } else {
+    //            qlines.push(line);
+    //        }
+    //    }
+
+    var startTime = new Date();
+    startTime = startTime.getTime();
+    var index = 0;
+    var nextLine = function () {
+        line = qlines[index];
+        index = index + 1;
+        if (line) {
+            line = line.trim();
+            console.log('>', line);
+            if (line.indexOf("use -c ") > -1) {
+                var ar = line.split(' ');
+                var url = ar[2];
+                connected = false;
+                var data = fs.readFileSync('tmp/' + S(url).slugify().s);
+                connected = true;
+                $ = cheerio.load(data);
+                nextLine();
+            } else
+            if (line.indexOf("use ") > -1) {
+                var ar = line.split(' ');
+                var url = ar[1];
+                connected = false;
+                console.log('connecting..');
+                request(url, function (error, response, body) {
+
+                    if (!error && response.statusCode == 200) {
+                        connected = true;
+                        $ = cheerio.load(body);
+                        console.log('connected');
+                        fs.writeFile('tmp/' + S(url).slugify().s, body);
+                        nextLine();
+
+                    }
+                })
+            } else
+            if (line == "exit") {
+                var now = new Date();
+                now = now.getTime();
+                var deltaTime = (now - startTime) / 1000;
+                console.log('executed ', deltaTime, 'seconds');
+                process.exit(0);
+            } else {
+                if (connected) {
+                    sandbox.send(line, function () {
+                        nextLine();
+                    });
+                } else {
+                    console.log('not connected');
+                    nextLine();
+                }
+            }
+            if (index >= qlines.length) {
+                var now = new Date();
+                now = now.getTime();
+                var deltaTime = (now - startTime) / 1000;
+                console.log('executed ', deltaTime, 'seconds');
+                process.exit(0);
+            }
+
+        }
+    }
+    nextLine();
 
 
+
+} else {
+    console.log("WSQL v.Alpha");
+    rl.setPrompt('WSQL> ');
+    rl.prompt();
+
+}
+// Shell
 rl.on('line', function (line) {
     line = line.trim();
+    if (line.indexOf("use -c ") > -1) {
+        var ar = line.split(' ');
+        var url = ar[2];
+        connected = false;
+
+        var data = fs.readFileSync('tmp/' + S(url).slugify().s);
+        connected = true;
+        $ = cheerio.load(data);
+
+    } else
     if (line.indexOf("use ") > -1) {
         var ar = line.split(' ');
         var url = ar[1];
         connected = false;
+        console.log('connecting..');
         request(url, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 connected = true;
-
+                console.log('connected');
                 $ = cheerio.load(body);
-
+                rl.prompt();
             }
         })
     } else
@@ -33,24 +147,13 @@ rl.on('line', function (line) {
         process.exit(0);
     } else {
         if (connected) {
-            var sandbox = {
-                web: new wsql($),
-                $:$,
-                LINK:'a',
-                TITLE:'h2',
-                LINKTITLE:'h2>a'
-            }
-            try {
-            var vmResult = vm.runInNewContext(line, sandbox);
-            console.log(vmResult);
-                } catch (e) {
-                    console.log(e);
-                }
+            sandbox.send(line);
         } else {
             console.log('not connected');
+            rl.prompt();
         }
     }
-    rl.prompt();
+
 }).on('close', function () {
     console.log('Have a great day!');
     process.exit(0);
--- wsql.js
+++ wsql.js
@@ -1,51 +1,144 @@
-
-const LINK = 'a',
-  TEXT = '*',
-  IMAGE = 'img'
-
-function wsQuery($,jqo, option, self) {
-  this.output = option;
-  this.element = jqo;
-  //ws.from('Kategori').
-  this.select = function(sel) {
-    if (!jqo) {
-      jqo = $('html');
+var S = require('string');
+var attributes=[];
+function wsQuery($, jqo, option, self) {
+    this.object = option;    
+    this.element = jqo;        
+    this.toJson=function(){
+        console.log(JSON.stringify(this.object));
     }
-    return new wsQuery($,jqo, sel);
-  }
-  this.from = function(text) {
-    if (!jqo) {
-      jqo = $('html');
-    }
-    var i = -2;
-    var self = jqo.find("*:contains(" + text + ")").eq(-1);
-    var el = jqo.find("*:contains(" + text + ")").eq(-2);
-    var i=-2;
-    while (el.find(option).length < 1) {
-      i=i-1;
-      el = jqo.find("*:contains(" + text + ")").eq(i);
-    }
-
-    return new wsQuery($,el, option, self);
-  }
-  this.fetch = function(attr) {
-    var ret = [];
-    if (!jqo) {
-      jqo = $('html');
-    }
-    var objects = jqo.find(option);
-    objects.each(function() {
-      if (self && $(this).text() != self.text()) {
-        if (!attr) {
-          ret.push($(this).text());
-        } else {
-          ret.push($(this).attr(attr));
+    this.getAttributes = function (sel) {
+        if (!option) {
+            return [];
         }
-      }
-    })
 
-    return new wsQuery($,objects, ret);
-  }
+        var sels = sel.split(',');
+        for (var e in sels) {
+            if (sels[e].indexOf(':attr-') > -1) {
+                var attribute = S(sels[e]).between(':attr-').s;
+                attributes.push(attribute);
+                sel = S(sel).replaceAll(':attr-' + attribute, '').s;
+            } else {
+                attributes.push(false);
+            }
+        }
+        return sel;
+    }
+    this.select = function (sel) {
+        if (!jqo) {
+            jqo = $('html');
+        }
+
+
+        return new wsQuery($, jqo, sel);
+    }
+    this.from = function (text) {
+        if (!jqo) {
+            jqo = $('html');
+        }
+        var o = '';
+        if (Array.isArray(option)) {
+            o = option.join().trim();
+        } else
+        if (typeof option === "object") {
+            o = [];
+            for (var k in option) {
+                o.push(option[k]);
+            }
+            o = o.join().trim();
+        }
+        o = this.getAttributes(o);
+        
+        if ($(text).length > 0) {
+            var el = $(text);
+            var self = $(text);
+        } else {
+            var i = -2;
+            var self = jqo.find("*:contains(" + text + ")").eq(-1);
+            var el = jqo.find("*:contains(" + text + ")").eq(-2);
+            var i = -2;
+            while (o && el.find(o).length < 1) {
+                i = i - 1;
+                el = jqo.find("*:contains(" + text + ")").eq(i);
+            }
+        }
+        return new wsQuery($, el, option, self);
+    }
+    this.fetch = function (attr) {
+        var me = this;
+        if (!jqo) {
+            jqo = $('html');
+        }
+        var sample = option;
+        if (Array.isArray(option)) {
+            sample = option[0];
+        } else
+        if (typeof option === "object") {
+            var values = [];
+            var keys = [];
+            for (var k in option) {
+
+                values.push(option[k]);
+                keys.push(k);
+            }
+            sample = values[0];
+            option = values;
+        }
+        var attribute = S(sample).between(':attr-').s;
+        sample = S(sample).replaceAll(':attr-' + attribute, '').s;
+        var list = jqo;
+        if (jqo.find('tr').length == jqo.find(sample).length) {
+            list = jqo.find('tr');
+        } else
+        if (jqo.find('li').length == jqo.find(sample).length) {
+            list = jqo.find('li');
+        } else
+        if (jqo.find('div').length == jqo.find(sample).length) {
+            list = jqo.find('div');
+        }
+
+        var records = [];
+
+        list.each(function () {
+            var th = $(this);
+            if (!Array.isArray(option)) {
+                option = [option];
+            }
+            for (var o in option) {
+                var opt = option[o];
+                var attribute = S(opt).between(':attr-').s;                
+                opt = S(opt).replaceAll(':attr-' + attribute, '').s;
+                var object = th.find(opt);
+                var ret = '';
+                if (self && object.text() != self.text()) {
+                    if (!attr && object) {                        
+                        if (attributes[o]) {
+                            ret = object.attr(attributes[o]);
+                        } else {
+                            if (object[0].name == "input") {
+                                ret = object.val();
+                            } else
+                            if (object[0].name == "img") {
+                                ret = object.attr("src");
+                            } else {
+                                ret = object.text();
+                            }
+                        }
+                    } else {
+                        ret = object.attr(attr);
+                    }
+                }
+                var f = {};
+                if (keys) {
+                    f[keys[o]] = ret;
+                } else {
+                    f[opt] = ret;
+                }
+                records.push(f);
+            }
+        })
+
+
+        return new wsQuery($, list, records);
+    }
 }
-module.exports =wsQuery;
-//web.select("h2").from("Keperluan Pribadi").fetch().output.join();
+module.exports = wsQuery;
